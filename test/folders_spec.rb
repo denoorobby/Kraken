16cbb301021bf96e20386e97f7225936a1d5b18f
Matthew Page <matthewnpage@gmail.com>
2012-03-19 18:24:00 -0700

    testy test

--- test/folders_spec.rb
+++ test/folders_spec.rb
@@ -2,12 +2,29 @@ require 'folders.rb'
 
 
 describe 'create folder' do
+
+  before(:each) { 
+    puts 'before'
+  }
+
+  after(:each) {
+    puts 'after'
+  }
+
   it 'given json , it should create a folder' do
 
     current_dir = File.dirname(__FILE__)
     settings = {}
-    createFolders(current_dir, '{"Folder1":""}', settings)
-    File.exists? 'Folder1'
+    createFolders(current_dir, '{"TestFolder":""}', settings)
+    File.exists? 'TestFolder'
+  end
+
+  it 'given a hash, it should create a folder' do
+    current_dir = File.dirname(__FILE__)
+    settings = {}
+    folders = { "TestFolder" => "" }
+    createFolders(current_dir, folders, settings) 
+    File.exists? 'TestFolder'
   end
 end
 
